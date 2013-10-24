4b86e31cd04459299aab4b3c2cff9581a89fda46
lhy <lhy@lhy-PC>
2013-10-24 22:29:11 +0800

    gege

---
+++ ZZ_Fragment/src/app/main/ui/wrong/WrongCallCode.java
@@ -0,0 +1,7 @@
+package app.main.ui.wrong;
+
+public class WrongCallCode {
+	public final static int BROWSE_CALL_NEW = 0;
+	public final static int LIST_CALL_NEW = 1;
+	public final static int LIST_CALL_BROWSE = 2;
+}
