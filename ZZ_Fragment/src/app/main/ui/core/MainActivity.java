2f0e044d438b62c49a84445f5c5a3f893f24a249
lhy <lhy@lhy-PC>
2013-10-27 13:05:36 +0800

    hehe

--- ZZ_Fragment/src/app/main/ui/core/MainActivity.java
+++ ZZ_Fragment/src/app/main/ui/core/MainActivity.java
@@ -4,19 +4,57 @@ import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
-import android.support.v4.app.FragmentTransaction;
+import android.view.View;
+import android.widget.Button;
 import app.main.R;
 
 public class MainActivity extends FragmentActivity {
+	private Button start;
+	private Button help;
+	private Button author;
+	private Button quit;
 
 	protected void onCreate(Bundle arg0) {
 		super.onCreate(arg0);
 		setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
 		setContentView(R.layout.main_center);
-
-		Intent intent = new Intent(MainActivity.this, SubjectsActivity.class);
-		startActivity(intent);
-
+		start = (Button) findViewById(R.id.main_start_btn);
+		help = (Button) findViewById(R.id.main_help_btn);
+		author = (Button) findViewById(R.id.main_author_btn);
+		quit = (Button) findViewById(R.id.main_quit_btn);
+		setListener();
 	}
 
+	private void setListener() {
+		start.setOnClickListener(new View.OnClickListener() {
+
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				Intent intent = new Intent(MainActivity.this,
+						SubjectsActivity.class);
+				startActivity(intent);
+			}
+		});
+		help.setOnClickListener(new View.OnClickListener() {
+
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+
+			}
+		});
+		author.setOnClickListener(new View.OnClickListener() {
+
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+
+			}
+		});
+		quit.setOnClickListener(new View.OnClickListener() {
+
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+
+			}
+		});
+	}
 }
